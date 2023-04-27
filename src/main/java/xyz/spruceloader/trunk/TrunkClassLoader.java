bcd776048edb6973825a7fb3d209845b856cb335
xtrm <oss@xtrm.me>
2023-04-28 01:41:48 +0200

    chore(main): applyLicenses

--- src/main/java/xyz/spruceloader/trunk/TrunkClassLoader.java
+++ src/main/java/xyz/spruceloader/trunk/TrunkClassLoader.java
@@ -1,3 +1,18 @@
+/*
+ * Trunk, the Spruce service used to launch Minecraft
+ * Copyright (C) 2023  SpruceLoader
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 package xyz.spruceloader.trunk;
 
 import org.apache.commons.io.IOUtils;
